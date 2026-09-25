9b4f2f71ced66615a802593f49f324a52f45c6ab
dositadi <akindivine587@gmail.com>
2026-09-25 16:56:31 +0100

    added platform crate

--- crates/hashroute/src/internal/platform/application.rs
+++ crates/hashroute/src/internal/platform/application.rs
@@ -1,3 +1,7 @@
+use anyhow::Result;
+use axum::{ Router, http::{ StatusCode }, response::IntoResponse, routing::get };
+use tokio::net::TcpListener;
+
 use crate::internal::AppConfig;
 
 #[derive(Clone, Debug)]
@@ -6,5 +10,17 @@ struct Application {
 }
 
 impl Application {
-    
+    pub async fn start_server(self) -> Result<()> {
+        let listener = TcpListener::bind(self.config.server.to_addr()).await?;
+
+        let router = Router::new().route("/", get(livez));
+
+        axum::serve(listener, router).await?;
+
+        Ok(())
+    }
+}
+
+async fn livez() -> impl IntoResponse {
+    StatusCode::OK
 }
